bdfb89732ed7a656fc993b8fe10aa8270ee86f1c
Mikołaj Cięszczyk <mikolaj.cieszczyk@stxnext.pl>
2024-04-23 21:19:19 +0200

    master: Fixed connection with db, added Database module to NestJS

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '../../.env' })],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'my_sql_container',
+      port: 3306,
+      username: 'root',
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
