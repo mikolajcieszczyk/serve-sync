bcf08d0b631c846648608c9de794e365012e3448
Mikołaj Cięszczyk <mikolaj.cieszczyk@stxnext.pl>
2024-06-17 18:34:08 +0200

    added logo

--- frontend/components/Layout/components/Navbar.tsx
+++ frontend/components/Layout/components/Navbar.tsx
@@ -1,11 +1,21 @@
 import { Box } from "#components/Box/Box.tsx";
 import { Typography } from "#components/Typography/Typography.tsx";
-
+import Image from "next/image";
+import Logo from "public/img/serve_sync_logo.png";
 export function Navbar() {
   return (
-    <Box className="w-64 p-4 flex flex-col bg-white">
+    <Box className="w-64 p-4 flex flex-col bg-white items-start">
       <aside className="">
-        <Typography variant="h3">ServeSync</Typography>
+        <div className="flex items-center justify-items-end">
+          <Image
+            src={Logo}
+            width={40}
+            height={40}
+            alt="serve sync logo"
+            placeholder="blur"
+          />
+          <Typography variant="h3">ServeSync</Typography>
+        </div>
         <nav>
           <ul>asasas</ul>
         </nav>
