ddb5eb416f4f15bc7900618b7f5481b98d9fd7ab
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2024-08-29 09:22:24 +0200

    fixed async error from console in dashboard/page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@nextui-org/react';
 import withAuth from 'HOC/withAuth';
 import { signOut } from 'next-auth/react';
 
-async function Page() {
+function Page() {
   return (
     <div className='flex w-1/4 flex-col'>
       <span>/dashboard </span>
